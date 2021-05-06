0ee360841c619de106b76c066973df236b99142e
sblum55 <healysa55@gmail.com>
2021-05-06 12:56:40 -0400

    updated seeders

--- seeders/20210506043622-products.js
+++ seeders/20210506043622-products.js
@@ -6,7 +6,7 @@ module.exports = {
       await queryInterface.bulkInsert('products', [{
         name: 'T-shirt',
         description: '100% Ring-spun cotton',
-        image: 'test.com',
+        image: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F3c%2Fa4%2F3ca4f91b[…]IFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]',
         price: 9.99,
         createdAt: new Date(),
         updatedAt: new Date()
@@ -14,7 +14,7 @@ module.exports = {
       {
         name: 'Tank top',
         description: '100% Ring-spun cotton',
-        image: 'test.com',
+        image: 'https://cdn.shopify.com/s/files/1/1464/5034/products/LUX-TANK-TOP-GREY.jpg?v=1615591321',
         price: 11.99,
         createdAt: new Date(),
         updatedAt: new Date()
