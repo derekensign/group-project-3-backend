306f7f4e86694b64f8dd3170eef41aa4bb849c11
Derek Ensign <derekensign@gmail.com>
2021-05-06 14:12:46 -0500

    added cart db table

--- models/cart.js
+++ models/cart.js
@@ -3,25 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class product extends Model {
+  class cart extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.product.belongsToMany(models.user, {through: 'cart'})
+
       // define association here
     }
   };
-  product.init({
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    image: DataTypes.TEXT,
-    price: DataTypes.DECIMAL(10,2)
+  cart.init({
+    userId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'product',
+    modelName: 'cart',
   });
-  return product;
+  return cart;
 };
